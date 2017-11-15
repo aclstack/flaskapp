35f590a954b352d078d12caeb57105955eb66951
liyongli <liyongli@wesai.com>
2017-11-15 16:55:51 +0800

    first commit

---
+++ flaskapp.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/admin')
+def admin():
+    return 'This is admin'
+
+
+if __name__ == '__main__':
+    app.run()
