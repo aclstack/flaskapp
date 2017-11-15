d19f7de05d4b4286e107d5062abda4f828626f4b
liyongli <liyongli@wesai.com>
2017-11-15 18:25:54 +0800

    瞎改

--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,43 @@
-from flask import Flask
+# -*- coding:utf-8 -*-
+from flask import Flask, request, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    #return 'Hello World!'
+    return "反向路由" + url_for('query')
+
+
+@app.route('/user', methods=['post'])
+def usr():
+    return 'This is post'
+
+
+# http://127.0.0.1:5000/user/12345
+@app.route('/user/<num>')
+def user_id(num):
+    return 'Hello' + num
+
+
+# http://127.0.0.1:5000/query?id=12345
+@app.route('/query')
+def query():
+    num = request.args.get('id')
+    return 'you input id is: ' + num
+
+
+@app.route('/ad')
+def ad():
+    return 'ok'
+
+# 反向路由
+@app.route('/route')
+def route():
+    #return "反向路由" + url_for('query')
+    return '123456'
+
 
 
 if __name__ == '__main__':
