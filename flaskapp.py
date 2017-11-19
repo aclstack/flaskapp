9a8c3b739bd962814ca3f2d699ba0f11b48f664b
aclstack <aclstack@gmail.com>
2017-11-19 16:09:58 +0800

    测试

--- flaskapp.py
+++ flaskapp.py
@@ -1,13 +1,19 @@
 # -*- coding:utf-8 -*-
-from flask import Flask, request, url_for, render_template
+from flask import Flask, request, url_for, render_template, flash, abort
 from models import User
+
+# 让flash支持中文输出
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 app = Flask(__name__)
+app.secret_key = '1234'
 
 
 @app.route('/')
 def hello_world():
-    world = "Hello World"
-    return render_template('index.html', world=world)
+    return render_template('index.html')
 
 
 @app.route('/user')
@@ -23,9 +29,13 @@ def usr_index():
 
 
 # http://127.0.0.1:5000/user/12345
-@app.route('/user/<num>')
-def user_id(num):
-    return 'Hello' + num
+@app.route('/user/<user_id>')
+def user_id(user_id):
+    if int(user_id) == 1:
+        flash('欢迎你 张三同学')
+        return render_template('user.html')
+    else:
+        abort(404)
 
 
 @app.route('/query_user/<user_id>')
@@ -55,6 +65,35 @@ def two():
     return render_template('two_base.html')
 
 
+@app.route('/login', methods=['post'])
+def login():
+    form = request.form
+    username = form.get('username')
+    password = form.get('password')
+    if not username:
+        flash('please input user')
+        return render_template('index.html')
+    if not password:
+        flash('please input password')
+        return render_template('index.html')
+
+    if username == 'admin' and password == '123456':
+        flash('Hello admin welcome to login')
+        return render_template('index.html')
+    else:
+        flash('username or password is error')
+    return render_template('index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
+
+
+@app.errorhandler(500)
+def not_found(e):
+    return render_template('500.html')
+
 # 反向路由
 @app.route('/route')
 def route():
@@ -64,4 +103,3 @@ def route():
 if __name__ == '__main__':
     app.run()
 
-#测试
