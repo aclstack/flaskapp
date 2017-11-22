0bdb691de8f43dfc1f305a3ebdb50d15c73fd320
aclstack <aclstack@gmail.com>
2017-11-22 09:55:12 +0800

    增加ORM方法

--- models.py
+++ models.py
@@ -1,13 +1,55 @@
 # -*- coding:utf-8 -*-
 from wtforms import Form, TextField, PasswordField, validators
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
-class User(object):
-    def __init__(self, user_id, username):
-        self.user_id = user_id
-        self.username = username
+# class User(object):
+#     def __init__(self, user_id, username):
+#         self.user_id = user_id
+#         self.username = username
 
 
 class LoginForm(Form):
     username = TextField("username", [validators.Required()])
     password = PasswordField("password", [validators.Required()])
+
+
+# 实例化一个对象
+app = Flask(__name__)
+# 设置数据库信息
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:123456@192.168.31.100/teacher"
+# 忽略警告
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# 生成db对象
+db = SQLAlchemy(app)
+
+
+# 继承了db的数据库模型，数据库模型提供了增删改查等一系列操作
+class User(db.Model):
+
+    id = db.Column(db.INTEGER, primary_key=True)
+    username = db.Column(db.String(32), unique=True)
+    password = db.Column(db.String(32))
+
+    def __init__(self, username, password):
+
+        self.username = username
+        self.password = password
+
+    def add(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return self.id
+        except Exception, e:
+            db.session.rollback()
+            return e.args[0]
+
+
+
+# 初始化表结构
+# db.create_all()
+u = User('Mike1234', '123456')
+print u.add()
+
